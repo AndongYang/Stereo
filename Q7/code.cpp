06fe53f1b185e403a91064104778c85f35ec572e
andongyang <42087538+andongyang@users.noreply.github.com>
2018-08-18 14:36:03 +0800

    Update code.cpp

--- Q7/code.cpp
+++ Q7/code.cpp
@@ -12,10 +12,11 @@ using namespace std;
 
 /*Created by yangandong*/
 
-const string g_read_camera_matrix_path = "camera_matrix.xml";
+const string g_read_camera_matrix_path = "camera_matrix_l.xml";
 const string g_read_photo_list_path = "left_photo_list.xml";
-const string g_save_path = "./res/";
+const string g_save_path = "./";
 
+//获取相机的相机矩阵与畸变参数
 bool get_camera_matrix_and_distCoeffs(Mat &camera_matrix, Mat &distortion_coeffs) {
 	FileStorage fs(g_read_camera_matrix_path, FileStorage::READ);
 	if (!fs.isOpened())
@@ -31,6 +32,7 @@ bool get_camera_matrix_and_distCoeffs(Mat &camera_matrix, Mat &distortion_coeffs
 	return true;
 }
 
+//获取待处理的图片列表
 vector<string> get_file_list() {
 	cout << "Strat read xml" << endl;
 	vector<string> file_list;
@@ -58,16 +60,21 @@ vector<string> get_file_list() {
 	return file_list;
 }
 
+//进行畸变矫正
 void do_undistort(vector<string> file_list, Mat camera_matrix, Mat distortion_coeffs) {
 	cout << "Start undistort" << endl;
 	int image_counter = 0;
+	//遍历图片列表，依次处理图片
 	for (image_counter = 0; image_counter < file_list.size(); image_counter++) {
+		//读取一张图片
 		Mat one_image = imread(file_list[image_counter]);
 		Mat one_image_res = one_image.clone();
 
+		//对图片进行畸变矫正
 		undistort(one_image, one_image_res, camera_matrix, distortion_coeffs);
 
-		string write_path = g_save_path + to_string(image_counter) + ".jpg";
+		//存储矫正后的图片
+		string write_path = g_save_path + "res_" + to_string(image_counter) + ".jpg";
 		imwrite(write_path, one_image_res);
 		cout << "Photo" << image_counter << " finished" << endl;
 	}
@@ -76,19 +83,22 @@ void do_undistort(vector<string> file_list, Mat camera_matrix, Mat distortion_co
 
 int main()
 {
-	Mat camera_matrix = Mat(3, 3, CV_32FC1, Scalar::all(0));       // Intrinsic matrix
-	Mat distortion_coeffs = Mat(1, 5, CV_32FC1, Scalar::all(0));   // Distortion factor[k1,k2,k3,p1,p2]
-	vector<Mat> rvecsMat;                                          // Extrinsic parameters R
-	vector<Mat> tvecsMat;										   // Extrinsic parameters T
+	Mat camera_matrix = Mat(3, 3, CV_32FC1, Scalar::all(0));       // 相机内参
+	Mat distortion_coeffs = Mat(1, 5, CV_32FC1, Scalar::all(0));   // 畸变参数[k1,k2,k3,p1,p2]
+	vector<Mat> rvecsMat;                                          // 旋转矩阵 R
+	vector<Mat> tvecsMat;										   // 位移矩阵 T
 
-	vector<string> file_list = get_file_list();					   // Get images
+	//获取待处理图片列表
+	vector<string> file_list = get_file_list();						
 
 	if (get_camera_matrix_and_distCoeffs(camera_matrix, distortion_coeffs)) {
+		 //如果获取相机相关参数成功，则进行畸变矫正
 		cout << "Read XML success." << endl;
 		do_undistort(file_list, camera_matrix, distortion_coeffs);
 	}
-	else
+	else //如果获取相机相关参数失败，则输出错误信息
 		cout << "Read XML failed." << endl;
 
+	system("pause");
 	return 0;
 }
